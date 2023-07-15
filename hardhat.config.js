28361b373144f639b72f4ff521106042baabbe12
Adam Kuruc <me@adamkuruc.dev>
2023-07-15 23:38:08 +0200

    created deploy script and deployed smart contract to localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
 require("@nomicfoundation/hardhat-toolbox");
 
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
   solidity: "0.8.19",
 };
